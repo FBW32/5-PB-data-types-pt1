21f18493899e44e6df028d73b9c6cca7dce90538
Gabriele Pepe <gabriele.pepe@gmail.com>
2020-08-05 15:45:45 +0200

    task completed

---
+++ Gabriele/dataTypes.js
@@ -0,0 +1,62 @@
+// 1. Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+
+let bestVeggie = "brokkoli";
+console.log("my favorite veggie is " + bestVeggie)
+
+// 2. Access the 2nd and 6th elements of the string. Print the elements to the console. Make sure you have selected the right index numbers
+
+console.log(bestVeggie[1]);
+console.log("the 2nd element of the string is" + " r");
+console.log(bestVeggie[5]);
+console.log("the 6th element of the string is" + " o");
+
+// 3. Declare a variable called name and assign a string as a value.
+
+let name = "Manfredi";
+console.log(name);
+
+// 4. Declare a variable called age and assign a number as a value.
+
+let age = 38;
+console.log(age);
+
+// 5. Declare a variable called year and assign a number as a value.
+
+let year = 1982;
+console.log(year);
+
+// 6. Print the following: "name is age in year", using your declared variables.
+
+console.log(name + " " + "is" + " " + age + " " + "in " + year);
+// Manfredi is 38 in 1982
+
+
+// 7. Declare a variable called isMarried and assign a boolean value.
+
+let isMarried = false;
+console.log(isMarried);
+
+// 8. Print the following "name is married: true/false", using your declared variables.
+
+console.log(name + " is married: " + isMarried); // Manfredi is married: false
+
+// 9. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true. Print "JavaScript is fun: true" to the console, using your declared variables.
+
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+
+
+// 10. Print the J and S of JavaScript from the variable programmingLanguage to the console.
+
+console.log(programmingLanguage[0]);
+console.log("J is index 0 in the word JavaScript");
+console.log(programmingLanguage[4]);
+console.log("S is index 4 in the word JavaScript");
+
+// 11. Print the length of the string "JavaScript" to the console.
+
+console.log(programmingLanguage.length); // 10
+console.log("The lenght of the word JavaScript is 10 characters");
+
