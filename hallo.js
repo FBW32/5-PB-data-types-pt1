7ca6d6df74f27abbb0cb327514ebb0e9ea886590
Maja <majamarek.design@gmail.com>
2020-08-05 16:32:17 +0200

    5th done

---
+++ hallo.js
@@ -0,0 +1,34 @@
+/* Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console. */
+let start = "sunnyDay";
+console.log(start);
+/* Access the 2nd and 6th elements of the string. Print the elements to the console. Make sure you have selected the right index numbers */
+console.log(start[1]);
+console.log(start[5]);
+/* Declare a variable called name and assign a string as a value. */
+let name = "Leonardo";
+console.log(name);
+/* Declare a variable called age and assign a number as a value.
+ */
+let age = 22;
+/* Declare a variable called year and assign a number as a value.
+ */
+let year = 2001;
+/* Print the following: "name is age in year", using your declared variables.
+ */
+console.log(name + " " + "is" + " " + age + " " + "in" + " " + year);
+/* Declare a variable called isMarried and assign a boolean value.
+ */
+let isMarried = true;
+/* Print the following "name is married: true/false", using your declared variables.
+ */
+console.log(name + " is married: " + isMarried);
+/* Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true. Print "JavaScript is fun: true" to the console, using your declared variables. */
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+/* Print the J and S of JavaScript from the variable programmingLanguage to the console.
+ */
+console.log(programmingLanguage[0], programmingLanguage[4]);
+/* Print the length of the string "JavaScript" to the console.
+ */
+console.log(programmingLanguage.length);
