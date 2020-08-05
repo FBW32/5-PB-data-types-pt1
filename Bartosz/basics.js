e7db819edd3a68567ac14846cfad60e4f9a1e85a
bartxs <b.soltysik90@gmail.com>
2020-08-05 15:44:57 +0200

    Exercise 5

---
+++ Bartosz/basics.js
@@ -0,0 +1,40 @@
+// Strings, Numbers, Booleans
+
+// Exercises to become familiar with data types.
+
+// 1. Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let string = "Paradox"
+console.log(string); // Paradox
+
+// 2. Access the 2nd and 6th elements of the string. Print the elements to the console. Make sure you have selected the right index numbers
+console.log("2nd element of string = " + string[2]); // 2nd element of string = r
+console.log("6nd element of string = " + string[6]); // 6nd element of string = x
+
+// 3. Declare a variable called name and assign a string as a value.
+let name = "Bart";
+
+// 4. Declare a variable called age and assign a number as a value.
+let age = 99;
+
+// 5. Declare a variable called year and assign a number as a value.
+let year = 1888;
+
+// 6. Print the following: "name is age in year", using your declared variables.
+console.log(name + " is " + age + " in " + year); // Bart is 99 in 1888
+
+// 7. Declare a variable called isMarried and assign a boolean value.
+let isMarried = false;
+
+// 8. Print the following "name is married: true/false", using your declared variables.
+console.log(name + " is married: " + isMarried); // Bart is married: false
+
+// 9. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true. Print "JavaScript is fun: true" to the console, using your declared variables.
+let programmingLanguage = "JavaScript"
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun); // JavaScript is fun: true
+
+// 10. Print the J and S of JavaScript from the variable programmingLanguage to the console.
+console.log(programmingLanguage[0], programmingLanguage[4] ); // J S
+
+// 11. Print the length of the string "JavaScript" to the console.
+console.log(programmingLanguage); // JavaScript
