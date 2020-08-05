69e0ec9fa645896a68111eb835192fba3c6551b1
frausafiye <s_gulenc@outlook.com>
2020-08-05 14:52:37 +0200

    added solutions

---
+++ exercise-5.js
@@ -0,0 +1,25 @@
+//1
+let myName = "Safiye";
+console.log("1." + myName);
+
+//2
+console.log("2." + myName[1] + "," + myName[5]);
+
+//3-4-5
+let name = "Mustafa";
+let age = 30;
+let year = 2020;
+//6
+console.log("6." + name + " is " + age + " in " + year);
+//7
+let isMarried = true;
+//8
+console.log("8." + name + " is married: " + isMarried);
+//9
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log("9." + programmingLanguage + " is fun: " + isFun);
+//10
+console.log("10." + programmingLanguage[0] +" " programmingLanguage[4]);
+//11
+console.log("11." + programmingLanguage.length);
