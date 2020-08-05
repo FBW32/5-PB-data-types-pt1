8c8ab6f871bd3e469946e45f004fdc4398f34165
Abayomi <mrbayus2014@gmail.com>
2020-08-06 01:29:58 +0200

    datatype solution

---
+++ datatypept1.js
@@ -0,0 +1,56 @@
+// 05.08.2020
+// EXERCISE 1
+//Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let city = "metropolitan";
+console.log("city");
+
+// EXERCISE 2
+// Access the 2nd and 6th elements of the string. Print the elements to the console. Make sure you have selected the right index numbers.
+console.log(city[2]);
+console.log(city[6]);
+
+// EXERCISE 3 
+// Declare a variable called name and assign a string as a value.
+let name = "Abayomi";
+
+
+// EXERCISE 4
+// Declare a variable called age and assign a number as a value.
+let age = 1;
+
+//EXERCISE 5
+//Declare a variable called year and assign a number as a value.
+let year = 2020;
+
+// EXERCISE 6
+// Print the following: "name is age in year", using your declared variables.
+console.log(name + " is " + age + " in " + year);
+
+// EXERCISE 7
+// Declare a variable called isMarried and assign a boolean value.
+let isMarried = true;
+
+// EXERCISE 8
+// Print the following "name is married: true/false", using your declared variables
+console.log(isMarried);
+
+// EXERCISE 9
+// Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true. Print "JavaScript is fun: true" to the console, using your declared variables.
+let programmingLanguage = "javascript";
+let isFun = true;
+console.log(programmingLanguage + "is" + isFun);  // Not sure how to do this
+
+
+// EXERCISE 10
+// Check whether mangoes added to apples is greater than oranges minus mangoes. If it is not, find an operator which will give a result of true
+console.log(programmingLanguage[0]);
+console.log(programmingLanguage[4]);
+
+
+// EXERCISE 11
+// Print the length of the string "JavaScript" to the console.
+console.log(programmingLanguage.length);
+
+
+
+
