0423db1c32c6d64fff3fbc9b36f861d067e9179f
Aiham <aiham.rabee@gmail.com>
2020-08-05 15:03:58 +0200

    maybe

---
+++ index.js
@@ -0,0 +1,24 @@
+let myString = "HereWE";
+console.log(myString);
+console.log("The 2nd element of the string is " + myString[1]); 
+console.log("The 6th element of the string is " + myString[5]);
+
+let name = "Aiham";
+let age = 31;
+let year = 2020;
+console.log(name + " is " + age + " in " + year);
+
+let isMarried = false;
+console.log(Boolean(name));
+
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+
+console.log(programmingLanguage[0]);
+console.log(programmingLanguage[4]);
+console.log(programmingLanguage.length);
+
+
+
