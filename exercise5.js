9bcbb4c4b1edc2b36af574148cbcb3f24afc6e65
shanti-s <surapanenishanti9@gmail.com>
2020-08-05 13:51:41 +0200

    Done

---
+++ exercise5.js
@@ -0,0 +1,20 @@
+let myString = "Exercises";
+console.log(myString); // Exercises
+console.log("The 2nd element of the string is " + myString[1]); // x
+console.log("The 6th element of the string is " + myString[5]); // i
+
+let name = "Eva";
+let age = 30;
+let year = 2020;
+console.log(name + " is " + age + " in " + year); // Eva is 30 in 2020
+
+let isMarried = true;
+console.log(Boolean(name)); // true
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun); // JavaScript is fun: true
+
+console.log(programmingLanguage[0]); // J
+console.log(programmingLanguage[4]); // S
+console.log(programmingLanguage.length); // 10
