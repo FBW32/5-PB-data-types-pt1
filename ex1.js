ea144ca6a3e39d8a289030232cba74908203eaff
Irina <irinagmarques@hotmail.com>
2020-08-05 14:45:14 +0200

    solution

---
+++ ex1.js
@@ -0,0 +1,23 @@
+// Programming Basics: Data Types Exercises Part 1
+
+let letters = "abcdefg";
+console.log(letters);
+console.log(letters[1]);
+console.log(letters[5]);
+
+let name = "Julia";
+let age = 27;
+let year = 2020;
+
+console.log(name + " is " + age + " in " + year);
+
+let  isMarried = false;
+console.log(name +" is married: " + isMarried);
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+
+console.log(programmingLanguage + " is fun: " + isFun);
+console.log(programmingLanguage[0]);
+console.log(programmingLanguage[4]);
+console.log(programmingLanguage.length);
